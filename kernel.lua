eab95a639b1d39c5446b925a9e990cc50b7a8f7c
lewislovesgames <lewisminecraft@hotmail.co.uk>
2015-03-12 16:24:35 +0000

    Update kernel.lua

--- kernel.lua
+++ kernel.lua
@@ -1,7 +1,26 @@
-_G.KernelInfo = {
+--# Kernel Main #--
+
+KernelInfo = {
   ["name"] = "JACCKA",
   ["os"] = "InsertNameOfOSHere",
-  ["quiet"] = false,
   ["ico"] = paintutils.loadImage("YourImageHere "), -- Has to be 3x3
   ["bootinto"] = "YourFileHere",
 }
+
+paintutils.drawImage( KernelInfo[ "ico" ] )
+term.setCursorPos( 1, 4 )
+print( "Starting " .. KernelInfo[ "os" ] )
+print( "Press F2 for Kernel Setup Utility. (5 seconds to press)" )
+parallel.waitForAny( 
+  function()
+    local count = 0
+    while count < 5 do
+      local event, scancode = os.pullEvent("key")
+      if keys.getName(scancode) == "f2" then
+        shell.run( "/kernel/setuputility" )
+      end
+      count = count + 1
+      sleep( 1 )
+    end
+  end
+)
