341b9909354023abf92b64b4b41f9d7707c2031a
lewislovesgames <lewisminecraft@hotmail.co.uk>
2015-03-12 08:40:02 +0000

    Create kernal.lua

---
+++ kernal.lua
@@ -0,0 +1,7 @@
+_G.KernalInfo = {
+  ["name"] = "CCKernal",
+  ["os"] = "InsertNameOfOSHere",
+  ["quiet"] = false,
+  ["ico"] = paintutils.loadImage("YourImageHere "), -- Has to be 3x3
+  ["bootinto"] = "YourFileHere",
+}
