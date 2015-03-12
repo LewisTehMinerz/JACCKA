bab9f7c792af4287435e4a812718321965068ee0
lewislovesgames <lewisminecraft@hotmail.co.uk>
2015-03-12 15:54:14 +0000

    Update and rename kernal.lua to kernel.lua

--- kernel.lua
+++ kernel.lua
@@ -1,5 +1,5 @@
-_G.KernalInfo = {
-  ["name"] = "CCKernal",
+_G.KernelInfo = {
+  ["name"] = "JACCKA",
   ["os"] = "InsertNameOfOSHere",
   ["quiet"] = false,
   ["ico"] = paintutils.loadImage("YourImageHere "), -- Has to be 3x3
