e5c7caf3ba86035d446c489373d0e2a6ab6eabfd
lewislovesgames <lewisminecraft@hotmail.co.uk>
2015-03-12 16:58:34 +0000

    Update kernel.lua

--- kernel.lua
+++ kernel.lua
@@ -1,26 +1,33 @@
---# Kernel Main #--
+--[[--
+  Name: JACCKA v0.1
+  Compyright (c) Lewislovesgames. DO NOT COPY.
+--]]--
 
 KernelInfo = {
-  ["name"] = "JACCKA",
-  ["os"] = "InsertNameOfOSHere",
-  ["ico"] = paintutils.loadImage("YourImageHere "), -- Has to be 3x3
-  ["bootinto"] = "YourFileHere",
+  [ "name" ] = "JACCKA v0.1",
+  [ "file" ] = "InsertNameOfFileHere",
+  [ "ico" ] = paintutils.loadImage("YourImageHere "), -- Has to be 3x3
 }
 
 paintutils.drawImage( KernelInfo[ "ico" ] )
 term.setCursorPos( 1, 4 )
-print( "Starting " .. KernelInfo[ "os" ] )
-print( "Press F2 for Kernel Setup Utility. (5 seconds to press)" )
-parallel.waitForAny( 
+print( "[kernel] Kernel: " KernelInfo[ "name" ] )
+print( "[kernel] Starting " .. KernelInfo[ "os" ] )
+print( "[stage] Inject Stage" )
+print( "[inject] Injecting kernel modifiers..." )
+parallel.waitForAll(
   function()
-    local count = 0
-    while count < 5 do
-      local event, scancode = os.pullEvent("key")
-      if keys.getName(scancode) == "f2" then
-        shell.run( "/kernel/setuputility" )
-      end
-      count = count + 1
-      sleep( 1 )
+    local f
+    if not fs.exists( "inject" ) then
+      f = fs.open( "inject", "w" )
+      f.write( "\n" )
+      f.close()
+      f = fs.open( KernelInfo[ "file" ], "a" )
+      f.write( " \n\nprint( \"[kernel] Shutdown \" ) \n\nsleep( 3 )" )
+      f.close()
+    else
+      print( "[inject] No need for injection. Skipping..." )
     end
   end
 )
+shell.run( KernelInfo[ "file" ] )
